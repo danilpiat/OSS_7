722e0f6dd417fead97aab1a771a5a9d859fd1d31
danilpiat <56876348+danilpiat@users.noreply.github.com>
2021-05-28 13:30:49 +0300

    oss_7

---
+++ Task_1.3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+extern char **environ;
+int main(int argc, char *argv[])
+{
+  char **p;
+  int vars = 0;
+  for (p = environ; *p != NULL; p++) 
+  { 
+    if(vars==10)
+	break;
+    else 
+	printf_s("%s\n", *p); 
+    vars++;
+  }
+}
